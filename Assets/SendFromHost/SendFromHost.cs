b4aac71db5253086e3d881e9ec31b307a762edc5
Seung-Tak Noh <seungtak.noh@gmail.com>
2025-09-04 17:17:49 +0900

    # minor fix on filename

--- Assets/SendFromHost/SendFromHost.cs
+++ Assets/SendFromHost/SendFromHost.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace MarchingCubes {
 
-sealed class DynamicFieldVisualizer : MonoBehaviour
+sealed class SendFromHost : MonoBehaviour
 {
     #region Editable attributes
 
